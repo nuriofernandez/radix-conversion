c49ab724f9e2606acfe812b30e416335c56f0143
Nurio Fernández <n.fernandez@efg.gg>
2024-08-18 19:07:06 +0200

    Moved common bases to a dedicated pkg

--- b10_to_hex_test.go
+++ b10_to_hex_test.go
@@ -1,57 +1,17 @@
 package radixconversion
 
 import (
-	"github.com/nuriofernandez/radix-conversion/base"
+	"github.com/nuriofernandez/radix-conversion/bases"
 	"github.com/nuriofernandez/radix-conversion/number"
 	"testing"
 )
 
-var base10 = base.Base{
-	Name: "Base 10",
-	Characters: map[int]rune{
-		0: '0',
-		1: '1',
-		2: '2',
-		3: '3',
-		4: '4',
-		5: '5',
-		6: '6',
-		7: '7',
-		8: '8',
-		9: '9',
-	},
-	Radix: 10,
-}
-
-var hexadecimal = base.Base{
-	Name: "Hexadecimal",
-	Characters: map[int]rune{
-		0:  '0',
-		1:  '1',
-		2:  '2',
-		3:  '3',
-		4:  '4',
-		5:  '5',
-		6:  '6',
-		7:  '7',
-		8:  '8',
-		9:  '9',
-		10: 'A',
-		11: 'B',
-		12: 'C',
-		13: 'D',
-		14: 'E',
-		15: 'F',
-	},
-	Radix: 16,
-}
-
 func TestDecimal17(t *testing.T) {
 	decimal := number.Number{
 		Value: "17",
-		Base:  base10,
+		Base:  bases.Decimal,
 	}
-	hex := decimal.ConvertTo(hexadecimal)
+	hex := decimal.ConvertTo(bases.Hexadecimal)
 	if hex.Value != "11" {
 		t.Fatalf("Incorrect result, expected '11' but received '%s'", hex.Value)
 		return
@@ -61,9 +21,9 @@ func TestDecimal17(t *testing.T) {
 func TestDecimal405(t *testing.T) {
 	decimal := number.Number{
 		Value: "405",
-		Base:  base10,
+		Base:  bases.Decimal,
 	}
-	hex := decimal.ConvertTo(hexadecimal)
+	hex := decimal.ConvertTo(bases.Hexadecimal)
 	if hex.Value != "195" {
 		t.Fatalf("Incorrect result, expected '195' but received '%s'", hex.Value)
 		return
